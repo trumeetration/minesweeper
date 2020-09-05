5d09d898c9a224169bd0156c9b90a11070d72cf4
trumeetration <war.naruto@mail.ru>
2020-09-06 02:48:46 +0700

    Added private fields: dimension, bombcount

--- Game.h
+++ Game.h
@@ -5,10 +5,11 @@ class Game {
         Field* field;
         bool isPlaying;
         Point koord;
+        int dim_x, dim_y, bCount;
     public:
         bool isRunning();
         int menu();
-        void drawField();
+        void drawField(); //+ 
         void renderAction();
-        void Start();
+        void Start(int dimension_x, int dimension_y, int bombCount); // +
 };
